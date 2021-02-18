8b6f47a0c1887b3a0a6701bac15b7d0fc9fbc62f
Serhii Diachok <sergey@sdyachok.com.ua>
2021-02-18 20:36:22 +0200

    Added basic home page and template

--- bookstrore/urls.py
+++ bookstrore/urls.py
@@ -1,4 +1,4 @@
-"""bookstrore URL Configuration
+"""Application URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import books.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', books.views.index, name='index')
 ]
