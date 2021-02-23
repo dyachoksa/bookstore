a08ca23d4e043629871c581b36e406fce72415ad
Serhii Diachok <sergey@sdyachok.com.ua>
2021-02-23 19:40:46 +0200

    Added basic search

--- books/views.py
+++ books/views.py
@@ -18,6 +18,22 @@ class BookListView(ListView):
     model = Book
     context_object_name = "books"
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        q = self.request.GET.get('q')
+        if q:
+            qs = qs.filter(title__icontains=q)
+
+        return qs
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+
+        context['q'] = self.request.GET.get('q')
+
+        return context
+
 
 class BookDetailView(DetailView):
     model = Book
